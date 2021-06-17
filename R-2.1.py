19fcd06237a4d6e4aa6f73e358ad2515441c5e78
Kadekraiii <58065230+Kadekraiii@users.noreply.github.com>
2021-06-17 12:41:52 +0800

    Add files via upload

---
+++ R-2.1.py
@@ -0,0 +1,15 @@
+#Berikan tiga contoh aplikasi perangkat lunak yang sangat penting bagi kehidupan.
+#Answer :
+#1. Microsoft Office
+"""Software ini sangat berguna untuk mempermudah atau meningkatkan efisiensi
+berbagai pekerjaan kantor, seperti perangkat lunak pengolah kata, pengolah
+angka (akuntansi), media presentasi, dan lain-lain. Contohnya seperti
+Microsoft Excel, Microsoft Word, Outlook, dan Powerpoint."""
+#2. Software Browser
+"""Sesuai namanya, software ini memiliki fungsi sebagai mesin pencari situs
+untuk mendapatkan berbagai informasi secara online. Beberapa di antaranya
+adalah Chrome, Opera Mini, Mozilla Firefox, Internet Explorer, dan sebagainya."""
+#3. Paint Software
+"""Software yang digunakan untuk mengolah gambar atau desain, mulai dari proses
+membuat suatu gambar hingga editing desain dapat dilakukan dengan beberapa
+perangkat lunak ini, yaitu Adobe Photoshop, Coreldraw, Paint, dll."""
