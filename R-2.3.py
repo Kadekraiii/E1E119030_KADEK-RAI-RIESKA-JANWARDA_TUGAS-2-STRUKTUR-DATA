10d494aaf71f67a191466d19f0b676195108de68
Kadekraiii <58065230+Kadekraiii@users.noreply.github.com>
2021-06-17 13:37:38 +0800

    Add files via upload

---
+++ R-2.3.py
@@ -0,0 +1,50 @@
+#Pushbutton
+"""Pushbutton merupakan jenis kontrol berupa tombol tekan yang akan menghasilkan 
+tindakan jika diklik, misanya tombol OK, Cancel, Hitung, Hapus, dan sebagainya."""
+
+#Toggle Button
+"""Toggle button memiliki fungsi yang sama dengan pushbutton. Perbedaanya adalah 
+saat pushbutton ditekan, maka tombol akan kembali pada posisi semula jika tombol 
+mouse dilepas, sedangkan pada toggle button, tombol tidak akan kembali ke posisi 
+semula, kecuali kita menekannya kembali."""
+
+#Radio Button
+"""Radio button digunakan untuk memilih atau menandai satu pilihan dari beberapa 
+pilihan yang ada. Misalnya, sewaktu kita membuat aplikasi operasi Matematika 
+(penjumlahan, pengurangan, perkalian, dan pembagian."""
+
+#Edit Text dan Static Text
+"""Edit text digunakan untuk memasukkan atau memodifikasi suatu text yang diinputkan 
+dari keyboard, sedangkan static text hanya berguna untuk menampilkan text/tulisan, 
+sehingga kita tidak bisa memodifikasi/mengedit text tersebut kecuali memalui property 
+inspector."""
+
+#Frames
+"""Frames merupakan kotak tertutup yang dapat digunakan untuk mengelompokkan kontrol-
+kontrol yang berhubungan. Tidak seperti kontrol lainnya, frames tidak memiliki rutin
+callback."""
+
+#Checkboxes
+"""Kontrol checkboxes berguna jika kita menyediakan beberapa pilihan mandiri atau tidak 
+bergantung dengan pilihan-pilihan lainnya. Contoh aplikasi penggunaan checkboxes adalah 
+ketika kita diminta untuk memilih hobi. Karena hobi bisa lebih dari satu, maka kita dapat 
+mengklik checkboxes lebih dari satu kali."""
+
+#Slider
+"""Slider berguna jika kita menginginkan inputan nilai tidak menggunakan keyboarad, tatapi 
+hanya dengan cara menggeser slider secara vertical maupun horizontal ke nilai yang kita 
+inginkan. Dengan menggunakan slider, kita lebih fleksibel dalam melakukan pemasukan nilai 
+data karena kita dapat mengatur sendiri nilai max, nilai min, serta sliderstep."""
+
+#Pop Up Menu
+"""Pop Up menu berguna menampilkan daftar pilihan yang didefinisikan pada String Propoerty 
+ketika mengklik tanda panah pada aplikasi dan memiliki fungsi yang sama seperti radio button. 
+Ketika tida dibukak, popup menu hanya menampilkan satu item yang menjadi pilihan pertama 
+pada String Property. Popupmenu sangat bermanfaat ketika kita ingin memberi sebuah pilihan 
+tanpa jarak, tidak seperti radiobutton."""
+
+#Axes
+"""Axes berguna untuk menampilkan sebuah grafik atau gambar (image). Axes sebenarnya tidak masuk 
+dalam UIControl, tetapi axes dapat deprogram agar pemakai dapat berinteraksi dengan axes dan 
+obyek grafik yang ditampilkan melalui axes."""
+
